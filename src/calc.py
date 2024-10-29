477a7c331dc234433428f0d8e4a31a26dd77d3c9
CrepeGoat <awqatty.b@gmail.com>
2024-10-29 02:11:55 -0600

    ruff format

--- src/calc.py
+++ src/calc.py
@@ -5,6 +5,7 @@ def add_bias(labeled_dist, bias: int):
     labeled_dist["x"] += bias
     return labeled_dist
 
+
 def kdn(k: int, n: int):
     # TODO make this more efficient by convolving together conv-exponent powers of 2
     dist = _take_index_n(_kdn_iter(n), k)
@@ -13,11 +14,13 @@ def kdn(k: int, n: int):
     labeled_dist["x"] += k
     return labeled_dist
 
+
 def _to_labeled(array: np.array):
     return dict(
         x=np.arange(len(array)),
         y=array.tolist(),
     )
+
 
 def _kdn_iter(n: int):
     dist_kdn = _0dn()
@@ -26,13 +29,16 @@ def _kdn_iter(n: int):
         yield dist_kdn
         dist_kdn = np.convolve(dist_kdn, dist_1dn)
 
+
 def _1dn(n: int):
-    result = np.full(n+1, fill_value=1/n)
+    result = np.full(n + 1, fill_value=1 / n)
     result[0] = 0
     return result
 
+
 def _0dn():
     return np.ones(1)
+
 
 def _take_index_n(iter, n: int):
     for _ in range(n):
--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
-
 import taipy.gui.builder as tgb
 from taipy.gui import Gui
 
 import calc
+
 
 # TODO allow for:
 # - heterogeneous dice
 # - multiplicative factors (e.g., double)
 def make_prob(d_count, d_sides, d_bias):
     return calc.add_bias(calc.kdn(d_count, d_sides), d_bias)
+
 
 def update_chart(state):
     state.d_prob = make_prob(int(state.d_count), int(state.d_sides), int(state.d_bias))
@@ -25,7 +26,9 @@ if __name__ == "__main__":
         tgb.text("# Getting started with Taipy GUI", mode="md")
         tgb.text("{int(d_count)}d{d_sides} + {int(d_bias)}")
         tgb.number("{d_count}", on_change=update_chart)
-        tgb.slider("{d_sides}",  lov=[2, 4, 6, 8, 10, 12, 20, 100], on_change=update_chart)
+        tgb.slider(
+            "{d_sides}", lov=[2, 4, 6, 8, 10, 12, 20, 100], on_change=update_chart
+        )
         tgb.number("{d_bias}", on_change=update_chart)
 
         tgb.chart(data="{d_prob}", x="x", y="y", type="bar")

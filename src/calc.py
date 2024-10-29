37d932bd4feba4d0fd55785903499dd1f22a0557
CrepeGoat <awqatty.b@gmail.com>
2024-10-29 03:38:14 -0600

    added "drop lowest/highest die" functionality

--- src/calc.py
+++ src/calc.py
@@ -1,4 +1,36 @@
+import math
+import functools
+
 import numpy as np
+
+
+def kdn_droplow(k: int, n: int):
+    result = kdn_drophigh(k, n)
+    result["y"] = np.flip(result["y"])
+    return result
+
+
+def kdn_drophigh(k: int, n: int):
+    dist = _kdn_drophigh(k, n) / (n**k)
+    dist_wo_leading_zeros = dist[k - 1 :]
+    labeled_dist = _to_labeled(dist_wo_leading_zeros)
+    labeled_dist["x"] += k - 1
+    return labeled_dist
+
+
+def _kdn_drophigh(k: int, n: int):
+    kdn_cache = [[x for (_, x) in zip(range(k), _dn_iter(m))] for m in range(n)]
+
+    return functools.reduce(
+        _add_raw,
+        (
+            _add_bias_raw(kdn_cache[n_fixed - 1][k - j], (j - 1) * n_fixed)
+            * math.comb(k, j)
+            for j in range(1, k + 1)  # j - the number of fixed dice
+            for n_fixed in range(1, n + 1)  # n_fixed - the value for fixed dice
+        ),
+        np.zeros(0),
+    )
 
 
 def add_bias(labeled_dist, bias: int):
@@ -8,7 +40,7 @@ def add_bias(labeled_dist, bias: int):
 
 def kdn(k: int, n: int):
     # TODO make this more efficient by convolving together conv-exponent powers of 2
-    dist = _take_index_n(_kdn_iter(n), k) / (n ** k)
+    dist = _take_index_n(_dn_iter(n), k) / (n**k)
     dist_wo_leading_zeros = dist[k:]
     labeled_dist = _to_labeled(dist_wo_leading_zeros)
     labeled_dist["x"] += k
@@ -22,7 +54,21 @@ def _to_labeled(array: np.array):
     )
 
 
-def _kdn_iter(n: int):
+def _add_bias_raw(dist, bias: int):
+    return np.pad(dist, (bias, 0))
+
+
+def _add_raw(dist1, dist2):
+    if len(dist1) > len(dist2):
+        dist1, dist2 = dist2, dist1
+
+    len_diff = len(dist2) - len(dist1)
+    assert len_diff >= 0
+
+    return dist2 + np.pad(dist1, (0, len_diff))
+
+
+def _dn_iter(n: int):
     dist_kdn = _0dn()
     dist_1dn = _1dn(n)
     while True:
@@ -31,6 +77,8 @@ def _kdn_iter(n: int):
 
 
 def _1dn(n: int):
+    if n == 0:
+        return np.zeros(1)
     result = np.full(n + 1, fill_value=1)
     result[0] = 0
     return result
--- src/main.py
+++ src/main.py
@@ -7,29 +7,40 @@ import calc
 # TODO allow for:
 # - heterogeneous dice
 # - multiplicative factors (e.g., double)
-def make_prob(d_count, d_sides, d_bias):
-    return calc.add_bias(calc.kdn(d_count, d_sides), d_bias)
+def make_prob(d_count, d_sides, d_bias, drop):
+    if drop == "high":
+        dist = calc.kdn_drophigh(d_count, d_sides)
+    elif drop == "low":
+        dist = calc.kdn_droplow(d_count, d_sides)
+    else:
+        dist = calc.kdn(d_count, d_sides)
+
+    return calc.add_bias(dist, d_bias)
 
 
 def update_chart(state):
-    state.d_prob = make_prob(int(state.d_count), int(state.d_sides), int(state.d_bias))
+    state.d_prob = make_prob(
+        int(state.d_count), int(state.d_sides), int(state.d_bias), state.drop
+    )
 
 
 if __name__ == "__main__":
     d_count = 1
     d_sides = 2
     d_bias = 0
-    d_prob = make_prob(d_count, d_sides, d_bias)
+    drop = "none"
+    d_prob = make_prob(d_count, d_sides, d_bias, drop)
 
     # Definition of the page
     with tgb.Page() as page:
         tgb.text("# Getting started with Taipy GUI", mode="md")
-        tgb.text("{int(d_count)}d{d_sides} + {int(d_bias)}")
+        tgb.text("{int(d_count)}d{d_sides} + {int(d_bias)} drop {drop}")
         tgb.number("{d_count}", on_change=update_chart)
         tgb.slider(
             "{d_sides}", lov=[2, 4, 6, 8, 10, 12, 20, 100], on_change=update_chart
         )
         tgb.number("{d_bias}", on_change=update_chart)
+        tgb.toggle("{drop}", lov=["low", "none", "high"], on_change=update_chart)
 
         tgb.chart(data="{d_prob}", x="x", y="y", type="bar")
 

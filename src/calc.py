f90a7281d61fb0b043f4be1ec64c7e00a34e9d0c
CrepeGoat <awqatty.b@gmail.com>
2024-10-29 01:15:55 -0600

    refactor labeling logic into separate function

--- src/calc.py
+++ src/calc.py
@@ -1,12 +1,18 @@
 import numpy as np
 
 
-# TODO make this more efficient by convolving together conv-exponent powers of 2
 def kdn_plusb(k: int, n: int, b: int):
-    y = _take_index_n(_kdn_iter(n), k).tolist()
+    # TODO make this more efficient by convolving together conv-exponent powers of 2
+    dist = _take_index_n(_kdn_iter(n), k)
+    dist_wo_leading_zeros = dist[k:]
+    labeled_dist = _to_labeled(dist_wo_leading_zeros)
+    labeled_dist["x"] += k + b
+    return labeled_dist
+
+def _to_labeled(array: np.array):
     return dict(
-        x=(np.arange(len(y) - k) + k + b).tolist(),
-        y=y[k:]
+        x=np.arange(len(array)),
+        y=array.tolist(),
     )
 
 def _kdn_iter(n: int):

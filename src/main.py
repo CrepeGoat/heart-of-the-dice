89998ac3f5a831155d064eb7015b64a2f778c228
CrepeGoat <awqatty.b@gmail.com>
2024-11-08 02:02:21 -0700

    changed UI to allow for arbitrary numbers of dropped dice

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import Literal
 
 import taipy.gui.builder as tgb
 from taipy.gui import Gui
@@ -9,21 +8,21 @@ import calc
 
 @dataclass(kw_only=True, slots=True)
 class DiceStruct(object):
-    _count: int
+    _count_keep: int
     _sides: int
-    drop: Literal["low", "none", "high"]
+    _count_drop: int
 
     def default():
-        return DiceStruct(_count=1, _sides=6, drop="none")
+        return DiceStruct(_count_keep=1, _sides=6, _count_drop=0)
 
     # UI stores numbers as floats -> auto-convert them to ints
     @property
-    def count(self):
-        return self._count
+    def count_keep(self):
+        return self._count_keep
 
-    @count.setter
-    def count(self, value):
-        self._count = int(value)
+    @count_keep.setter
+    def count_keep(self, value):
+        self._count_keep = int(value)
 
     @property
     def sides(self):
@@ -33,32 +32,59 @@ class DiceStruct(object):
     def sides(self, value):
         self._sides = int(value)
 
+    @property
+    def count_drop(self):
+        return self._count_drop
+
+    @count_drop.setter
+    def count_drop(self, value):
+        self._count_drop = int(value)
+
+
+def make_dice_label(count_keep, count_drop, sides, bias):
+    result = f"{die.count_keep + abs(die.count_drop)}d{die.sides}"
+
+    if die.count_drop < 0:
+        result += f" keep highest {die.count_keep}"
+    elif die.count_drop > 0:
+        result += f" keep lowest {die.count_keep}"
+
+    bias = int(bias)
+    if count_drop != 0 and bias != 0:
+        result = f"({result})"
+    if bias < 0:
+        result += f" - {abs(bias)}"
+    elif bias > 0:
+        result += f" + {bias}"
+
+    return result
+
 
 # TODO allow for:
 # - heterogeneous dice
 # - multiplicative factors (e.g., double)
-def calc_prob(d_count, d_sides, d_bias, drop):
+def calc_prob(keep, d_sides, d_bias, drop):
     dist1 = calc.roll_1dn(d_sides)
-    if drop == "high":
-        dist = calc.roll_k_drophigh(dist1, d_count, 1)
-    elif drop == "low":
-        dist = calc.roll_k_droplow(dist1, d_count, 1)
+    if drop > 0:
+        dist = calc.roll_k_drophigh(dist1, keep + drop, drop)
+    elif drop < 0:
+        dist = calc.roll_k_droplow(dist1, keep - drop, -drop)
     else:
-        dist = calc.roll_k(dist1, d_count)
+        dist = calc.roll_k(dist1, keep)
 
     return dist.bias_by(d_bias).scaled_to_prob().to_labeled()
 
 
 def update_chart(state):
     state.d_prob = calc_prob(
-        state.die.count, state.die.sides, int(state.d_bias), state.die.drop
+        state.die.count_keep, state.die.sides, int(state.d_bias), state.die.count_drop
     )
 
 
 if __name__ == "__main__":
     die = DiceStruct.default()
     d_bias = 0
-    d_prob = calc_prob(die.count, die.sides, d_bias, die.drop)
+    d_prob = calc_prob(die.count_keep, die.sides, d_bias, die.count_drop)
 
     chart_properties = dict(
         layout=dict(
@@ -71,23 +97,21 @@ if __name__ == "__main__":
     with tgb.Page() as page:
         tgb.text("# D&D Dice Calculator", mode="md")
 
-        with tgb.layout(columns="100px 300px 200px 100px"):
+        with tgb.layout(columns="100px 300px 100px 100px"):
             tgb.text("# of dice")
             tgb.text("# of sides")
             tgb.text("drop die")
             tgb.text("then add")
 
-            tgb.number("{die.count}", min=0, on_change=update_chart)
+            tgb.number("{die.count_keep}", min=0, on_change=update_chart)
             tgb.slider(
                 "{die.sides}", lov=[2, 4, 6, 8, 10, 12, 20, 100], on_change=update_chart
             )
-            tgb.toggle(
-                "{die.drop}", lov=["low", "none", "high"], on_change=update_chart
-            )
+            tgb.number("{die.count_drop}", on_change=update_chart)
             tgb.number("{d_bias}", on_change=update_chart)
 
         tgb.text(
-            "## -> {int(die.count)}d{die.sides} drop {die.drop} + {int(d_bias)}",
+            "## -> {make_dice_label(die.count_keep, die.count_drop, die.sides, d_bias)}",
             mode="md",
         )
         tgb.chart(

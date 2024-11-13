d566abeabb315d06cf63063638bb4553b44e6786
CrepeGoat <awqatty.b@gmail.com>
2024-11-12 23:03:46 -0700

    refactored s.t. minimal code is under `if __name__ == "__main__"`

--- src/main.py
+++ src/main.py
@@ -95,43 +95,46 @@ def buy_me_a_coffee_button():
     )
 
 
-if __name__ == "__main__":
-    die = DiceStruct.default()
-    d_bias = 0
-    d_prob = calc_prob(die.count_keep, die.sides, d_bias, die.count_drop)
+die = DiceStruct.default()
+d_bias = 0
+d_prob = calc_prob(die.count_keep, die.sides, d_bias, die.count_drop)
 
-    chart_properties = dict(
-        layout=dict(
-            xaxis=dict(title="dice outcome"),
-            yaxis=dict(title="probability (per whole)"),
+chart_properties = dict(
+    layout=dict(
+        xaxis=dict(title="dice outcome"),
+        yaxis=dict(title="probability (per whole)"),
+    )
+)
+
+# Definition of the page
+with tgb.Page() as page:
+    tgb.text("# D&D Dice Calculator", mode="md")
+
+    with tgb.layout(columns="100px 300px 100px 100px"):
+        tgb.text("# of dice")
+        tgb.text("# of sides")
+        tgb.text("drop die")
+        tgb.text("then add")
+
+        tgb.number("{die.count_keep}", min=0, on_change=update_chart)
+        tgb.slider(
+            "{die.sides}", lov=[2, 4, 6, 8, 10, 12, 20, 100], on_change=update_chart
         )
+        tgb.number("{die.count_drop}", on_change=update_chart)
+        tgb.number("{d_bias}", on_change=update_chart)
+
+    tgb.text(
+        "## -> {make_dice_label(die.count_keep, die.count_drop, die.sides, d_bias)}",
+        mode="md",
+    )
+    tgb.chart(
+        data="{d_prob}", x="x", y="y", type="bar", properties="{chart_properties}"
     )
 
-    # Definition of the page
-    with tgb.Page() as page:
-        tgb.text("# D&D Dice Calculator", mode="md")
+    buy_me_a_coffee_button()
 
-        with tgb.layout(columns="100px 300px 100px 100px"):
-            tgb.text("# of dice")
-            tgb.text("# of sides")
-            tgb.text("drop die")
-            tgb.text("then add")
 
-            tgb.number("{die.count_keep}", min=0, on_change=update_chart)
-            tgb.slider(
-                "{die.sides}", lov=[2, 4, 6, 8, 10, 12, 20, 100], on_change=update_chart
-            )
-            tgb.number("{die.count_drop}", on_change=update_chart)
-            tgb.number("{d_bias}", on_change=update_chart)
+app_gui = Gui(page)
 
-        tgb.text(
-            "## -> {make_dice_label(die.count_keep, die.count_drop, die.sides, d_bias)}",
-            mode="md",
-        )
-        tgb.chart(
-            data="{d_prob}", x="x", y="y", type="bar", properties="{chart_properties}"
-        )
-
-        buy_me_a_coffee_button()
-
-    Gui(page).run(debug=True)
+if __name__ == "__main__":
+    app_gui.run(debug=True)

e52c07f07850509183f7d62aac1d1bff6cc0e5dc
CrepeGoat <awqatty.b@gmail.com>
2024-11-16 20:58:05 -0700

    more testing: found some trends

--- dice/script_spline_kdn_droplow_km1.py
+++ dice/script_spline_kdn_droplow_km1.py
@@ -2,66 +2,15 @@ import numpy as np
 
 from dice import calc, calc_spline
 
-############
-k = 3
-drop = 1
-print(f"{k} drop {drop}")
-
-dist = calc.roll_k_droplow(calc.roll_1dn(8), k=k, drop=drop)
-print(f"\tprobs: {dist.seq}")
-
-coeffs = []
-gen = calc_spline._polynomial_inv_state_machine(np.zeros(k + 1, dtype=np.int64))
-next(gen)
-for i in dist.seq:
-    coeffs.append(gen.send(i))
-coeffs = np.array(coeffs, dtype=np.int64)
-print(f"\t{k+1}-degree poly: {coeffs}")
-
-coeffs_est = np.array([1, 0] + [(-1) ** i for i in range(13)], dtype=np.int64)
-print(f"\tpattern: {coeffs_est}")
-print(f"\tdiff from pattern: {coeffs - coeffs_est}")
-
-
-############
-k = 4
-drop = 2
-print(f"{k} drop {drop}")
-
-dist = calc.roll_k_droplow(calc.roll_1dn(8), k=k, drop=drop)
-print(f"\tprobs: {dist.seq}")
-
-coeffs = []
-gen = calc_spline._polynomial_inv_state_machine(np.zeros(k + 1, dtype=np.int64))
-next(gen)
-for i in dist.seq:
-    coeffs.append(gen.send(i))
-coeffs = np.array(coeffs, dtype=np.int64)
-print(f"\t{k+1}-degree poly: {coeffs}")
-
-coeffs_est = np.array(
-    [1, 0] + [4 * (i + 1) * ((-1) ** i) for i in range(13)], dtype=np.int64
-)
-print(f"\tpattern: {coeffs_est}")
-print(f"\tdiff from pattern: {coeffs - coeffs_est}")
-
-
-print("\n\n\n")
-
 for k in range(3, 10):
     drop = k - 2
     print(f"{k} drop {drop}")
 
-    dist = calc.roll_k_droplow(calc.roll_1dn(20), k=k, drop=drop)
+    dist = calc.roll_k_droplow(calc.roll_1dn(21), k=k, drop=drop)
     print(f"\tprobs: {dist.seq}")
 
-    coeffs = []
-    gen = calc_spline._polynomial_inv_state_machine(np.zeros(k + 1, dtype=np.int64))
-    next(gen)
-    for i in dist.seq:
-        coeffs.append(gen.send(i))
-    coeffs = np.array(coeffs, dtype=np.int64)
-    print(f"\t{k+1}-degree poly: {coeffs}")
+    coeffs = calc_spline._poly_inv(dist.seq.astype(np.int64), k)
+    print(f"\t{k}-degree poly: {coeffs}")
 
     coeffs_abs = np.abs(coeffs)
     print(f"\t abs: {coeffs_abs}")

c5e3690eefe2523ad4d349ef04e07481e0ef4c78
CrepeGoat <awqatty.b@gmail.com>
2025-03-31 00:32:06 -0600

    finished 3.2 dynamic attributes

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,15 @@ fn App() -> impl IntoView {
     let (count, set_count) = signal(0);
 
     view! {
-        <button on:click=move |_| {
-            *set_count.write() += 1;
-        }>"Click me: " {count}</button>
-        <p>"Double count: " {move || count.get() * 2}</p>
+        <button
+            on:click=move |_| {
+                *set_count.write() += 1;
+            }
+            // the class: syntax reactively updates a single class
+            // here, we'll set the `red` class when `count` is odd
+            class:red=move || count.get() % 2 == 1
+        >
+            "Click me"
+        </button>
     }
 }

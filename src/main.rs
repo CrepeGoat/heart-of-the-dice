c32bdbd767e9ba0732d96c0d75d74ceeb2d21479
CrepeGoat <awqatty.b@gmail.com>
2025-03-31 00:24:15 -0600

    finished 3.1 a basic component

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
-use leptos::prelude::*;
+use leptos::mount::mount_to_body;
 
 fn main() {
-    leptos::mount::mount_to_body(|| view! { <p>"Hello, world!"</p> })
+    mount_to_body(App);
+}
+
+use leptos::prelude::*;
+
+#[component]
+fn App() -> impl IntoView {
+    let (count, set_count) = signal(0);
+
+    view! {
+        <button on:click=move |_| {
+            *set_count.write() += 1;
+        }>"Click me: " {count}</button>
+        <p>"Double count: " {move || count.get() * 2}</p>
+    }
 }

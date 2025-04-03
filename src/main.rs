b478d2e8bfabe41eae32207b306864cf5768598f
CrepeGoat <awqatty.b@gmail.com>
2025-04-03 13:39:25 -0600

    wip: added poc chart via leptos-chartistry

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use leptos::prelude::*;
+use leptos_chartistry::*;
 use std::num::ParseIntError;
 
 fn main() {
@@ -15,8 +16,42 @@ fn main() {
                 adv_dice_count=adv_dice_count
                 modifier=modifier
             />
+            <ProbabilityChart/>
         }
     });
+}
+
+#[component]
+fn ProbabilityChart() -> impl IntoView {
+    struct TestDatum {
+        x: f64,
+        y: f64,
+    }
+    let test_data = RwSignal::new(vec![
+        TestDatum { x: 0., y: 0.25 },
+        TestDatum { x: 1., y: 0.5 },
+        TestDatum { x: 2., y: 0.25 },
+    ]);
+    let series = Series::new(|data: &TestDatum| data.x)
+        .bar(Bar::new(|data: &TestDatum| data.y).with_placement(BarPlacement::Zero));
+    view! {
+        <Chart
+            aspect_ratio=AspectRatio::from_outer_height(300.0, 1.2)
+            series=series
+            data=test_data
+            debug=true
+
+            left=TickLabels::aligned_floats()
+            inner=[
+                AxisMarker::left_edge().into_inner(),
+                AxisMarker::bottom_edge().into_inner(),
+                // XGridLine::default().into_inner(),
+                YGridLine::default().into_inner(),
+                YGuideLine::over_mouse().into_inner(),
+                // XGuideLine::over_data().into_inner(),
+            ]
+        />
+    }
 }
 
 #[component]

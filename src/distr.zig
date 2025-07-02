4eb35c7f963dfe51d72fdd7d7cc5c689cc7df5a4
CrepeGoat <awqatty.b@gmail.com>
2025-07-02 16:06:49 -0600

    wip: adding tests for distribution math

--- src/distr.zig
+++ src/distr.zig
@@ -1,5 +1,78 @@
 const std = @import("std");
 
+test "CountDiceOutcomes.roll0" {
+    const CDO = CountDiceOutcomes(i32, usize, u64);
+    const allocator = std.testing.allocator;
+
+    const result = try CDO.roll0(allocator);
+    defer result.deinit(allocator);
+    try std.testing.expectEqual(0, result.index_first);
+    try std.testing.expectEqualSlices(u64, &[_]u64{1}, result.seq);
+}
+
+test "CountDiceOutcomes.roll1dn" {
+    const CDO = CountDiceOutcomes(i32, usize, u64);
+    const allocator = std.testing.allocator;
+
+    inline for (0..10) |n| {
+        const result = try CDO.roll1dn(allocator, @intCast(n));
+        defer result.deinit(allocator);
+
+        try std.testing.expectEqual(1, result.index_first);
+        try std.testing.expectEqualSlices(u64, &([_]u64{1} ** n), result.seq);
+    }
+}
+
+test "CountDiceOutcomes - roll4d6" {
+    const CDO = CountDiceOutcomes(i32, usize, u64);
+    const allocator = std.testing.allocator;
+
+    const r1 = try CDO.roll1dn(allocator, 6);
+    defer r1.deinit(allocator);
+    const result = try CDO.rollKTimes(allocator, r1, 4);
+    defer result.deinit(allocator);
+
+    const brute_result = try test_brute_force_distr_4d6(allocator, struct {
+        pub fn f(v1: u64, v2: u64, v3: u64, v4: u64) u64 {
+            return v1 + v2 + v3 + v4;
+        }
+    }.f);
+    defer brute_result.deinit(allocator);
+
+    try std.testing.expectEqual(brute_result.index_first, result.index_first);
+    try std.testing.expectEqualSlices(u64, brute_result.seq, result.seq);
+}
+
+fn test_brute_force_distr_4d6(
+    allocator: std.mem.Allocator,
+    mapFn: anytype,
+) !SequenceWithOffset(usize, u64) {
+    const dice_sides: u64 = 6;
+    var values = std.ArrayList(u64).init(allocator);
+
+    for (1..dice_sides + 1) |v1| {
+        for (1..dice_sides + 1) |v2| {
+            for (1..dice_sides + 1) |v3| {
+                for (1..dice_sides + 1) |v4| {
+                    const value = mapFn(v1, v2, v3, v4);
+                    const value_usize = @as(usize, @intCast(value));
+                    if (value_usize >= values.items.len) {
+                        try values.appendNTimes(0, 1 + value_usize - values.items.len);
+                    }
+                    values.items[value_usize] += 1;
+                }
+            }
+        }
+    }
+
+    const i = for (0..values.items.len) |i| {
+        if (values.items[i] != 0) break i;
+    } else values.items.len;
+    try values.replaceRange(0, i, &[0]u64{});
+
+    return .{ .index_first = i, .seq = try values.toOwnedSlice() };
+}
+
 pub fn CountDiceOutcomes(D: type, X: type, Y: type) type {
     const SeqWOffset = SequenceWithOffset(X, Y);
     const Result = std.mem.Allocator.Error!SeqWOffset;
@@ -15,13 +88,17 @@ pub fn CountDiceOutcomes(D: type, X: type, Y: type) type {
             return SeqWOffset{ .index_first = 1, .seq = buffer };
         }
 
-        pub fn rollKTimes(allocator: std.mem.Allocator, roll1: SeqWOffset, k: D) Result {
-            const result = try roll0(allocator);
-            const result_tmp = undefined;
+        pub fn rollKTimes(
+            allocator: std.mem.Allocator,
+            roll1: SeqWOffset,
+            k: D,
+        ) (std.mem.Allocator.Error || error{Overflow})!SeqWOffset {
+            var result = try roll0(allocator);
+            var result_tmp: SeqWOffset = undefined;
 
-            for (0..k) |_| {
+            for (0..@intCast(k)) |_| {
                 std.mem.swap(SeqWOffset, &result_tmp, &result);
-                result = result_tmp.addDistr(allocator, roll1);
+                result = try result_tmp.addDistr(allocator, roll1);
 
                 result_tmp.deinit(allocator);
                 result_tmp = undefined;
@@ -35,7 +112,7 @@ pub fn CountDiceOutcomes(D: type, X: type, Y: type) type {
             roll1: SeqWOffset,
             dice_count: D,
             drop_count: D,
-        ) Result {
+        ) std.mem.Allocator.Error!SeqWOffset {
             const inner_func = struct {
                 pub fn func(
                     n: X,
@@ -148,16 +225,13 @@ test "SequenceWithOffset.addValues" {
 
 test "SequenceWithOffset.biasBy" {
     const SeqWOffset = SequenceWithOffset(isize, i32);
-    const allocator = std.testing.allocator;
 
     var buffer = [_]i32{ 1, 2, 3, 4 };
-    const seq = SeqWOffset{ .index_first = 1, .seq = &buffer };
+    var seq = SeqWOffset{ .index_first = 1, .seq = &buffer };
+    try seq.biasBy(3);
 
-    const result = seq.biasBy(allocator, 3);
-
-    try std.testing.expectEqual(&seq, &result);
-    try std.testing.expectEqual(4, result.index_first);
-    try std.testing.expectEqualSlices(i32, &[_]i32{ 1, 2, 3, 4 }, result.seq);
+    try std.testing.expectEqual(4, seq.index_first);
+    try std.testing.expectEqualSlices(i32, &[_]i32{ 1, 2, 3, 4 }, seq.seq);
 }
 
 test "SequenceWithOffset.applyFnToValues" {
@@ -181,14 +255,12 @@ test "SequenceWithOffset.applyFnToValues" {
 
 test "SequenceWithOffset.scaleBy" {
     const SeqWOffset = SequenceWithOffset(isize, i32);
-    const allocator = std.testing.allocator;
 
     var buffer = [_]i32{ 1, 2, 3, 4 };
-    const seq = SeqWOffset{ .index_first = 1, .seq = &buffer };
-    const result = seq.scaleBy(allocator, 5);
+    var seq = SeqWOffset{ .index_first = 1, .seq = &buffer };
+    try seq.scaleBy(5);
 
-    try std.testing.expectEqual(1, result.index_first);
-    try std.testing.expectEqualSlices(i32, &[_]i32{ 5, 10, 15, 20 }, result.seq);
+    try std.testing.expectEqualSlices(i32, &[_]i32{ 5, 10, 15, 20 }, seq.seq);
 }
 
 /// A sequence of numbers, offset from zero by a set amount.
@@ -232,10 +304,14 @@ pub fn SequenceWithOffset(X: type, Y: type) type {
             allocator.free(self.seq);
         }
 
-        pub fn addDistr(self: Self, allocator: std.mem.Allocator, other: Self) Result {
+        pub fn addDistr(
+            self: Self,
+            allocator: std.mem.Allocator,
+            other: Self,
+        ) (std.mem.Allocator.Error || error{Overflow})!Self {
             return .{
                 .index_first = self.index_first + other.index_first,
-                .seq = convolve1d(
+                .seq = try convolve1d(
                     Y,
                     try allocator.alloc(Y, self.seq.len + other.seq.len - 1),
                     self.seq,
@@ -265,16 +341,14 @@ pub fn SequenceWithOffset(X: type, Y: type) type {
             return .{ .index_first = index_low, .seq = seq };
         }
 
-        pub fn biasBy(self: Self, bias: X) (error{Overflow}!Self) {
-            self.index_first = try std.math.add(self.index_first, bias);
-            return self;
+        pub fn biasBy(self: *Self, bias: X) (error{Overflow}!void) {
+            self.index_first = try std.math.add(X, self.index_first, bias);
         }
 
-        pub fn scaleBy(self: Self, scale: Y) (error{Overflow}!Self) {
+        pub fn scaleBy(self: *Self, scale: Y) (error{Overflow}!void) {
             for (0..self.seq.len) |i| {
-                self.seq[i] = try std.math.mul(self.seq[i], scale);
+                self.seq[i] = try std.math.mul(Y, self.seq[i], scale);
             }
-            return self;
         }
 
         pub fn applyFnToValues(
@@ -321,8 +395,9 @@ fn convolve1d(comptime T: type, result: []T, a1: []const T, a2: []const T) (erro
         _result[i] = 0;
         for (0..a1sub.len) |j| {
             result[i] = try std.math.add(
+                T,
                 result[i],
-                try std.math.mul(a1sub[j], a2sub[a1sub.len - j - 1]),
+                try std.math.mul(T, a1sub[j], a2sub[a1sub.len - j - 1]),
             );
         }
     }
